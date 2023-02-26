3656eaf2ed20a38be7f606d789982235956b3d7b
jininono <jininono96@gmail.com>
2023-02-26 17:54:39 +0900

    2023-02-26 commit

--- js/index2.js
+++ js/index2.js
@@ -26,24 +26,30 @@ $(function(){
 	});
 
 	//gnb 클릭 시 이동
-	$('.gnb_a').on('click',function(e){
+	// $('.gnb_a').on('click',function(e){
 
-		//URL에 #id 표시되지 않도록
-		e.preventDefault();
-		$('html,body').animate({scrollTop:$(this.hash).offset().top},1000,'swing');
+	// 	//URL에 #id 표시되지 않도록
+	// 	e.preventDefault();
 
-		if($(this).hasClass('active-color')==true){ 
-			$(this).removeClass('active-color');
+	// 	// $('html, body').animate({
+	//     //     scrollTop: $($.attr(this, 'href')).offset().top
+	//     // }, 500);
+	// 	// $('html,body').animate({scrollTop:$(this.hash).offset().top},1000,'swing');
+	
 
-		}else{
-			$('.gnb_a').removeClass('active-color');
-			$(this).addClass('active-color');
-		}
+	// 	if($(this).hasClass('active-color')==true){ 
+	// 		$(this).removeClass('active-color');
 
-	});
+	// 	}else{
+	// 		$('.gnb_a').removeClass('active-color');
+	// 		$(this).addClass('active-color');
+	// 	}
+
+	// });
 
 	//fullpage
 	$('#fullpage').fullpage ({
+		//anchors: ['home', 'about', 'abillty', 'portfolio', 'contact'],
 		autoScrolling: true,
 		scrollHorizontally: true,
 		navigation: true,
@@ -51,26 +57,68 @@ $(function(){
 		responsiveWidth: 1024
 	});
 
+	function vh(v) {
+  		var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+  		return (v * h) / 100;
+	}
+
+	// $(window).resize(function () {
+	// 	console.log(vh(100));
+
+	// 	if (window.innerWidth < 1024) {
+
+	// 		$(window).on("scroll", function () {
+
+	// 			var scroll = $(window).scrollTop();
+
+	// 			if (scroll > 100) {
+	// 				$('header').css('background','#fff');
+	// 			} else {
+	// 				$('header').css('background','none');
+	// 			}
+	// 		});
+	// 	} else {
+	// 		$('header').css('background','none');
+
+	// 		$(window).on("scroll", function () {
+
+	// 			var scroll = $(window).scrollTop();
+
+	// 			if (scroll > vh(100)) {
+	//         		$('.gnb_a').addClass('active-color');
+	// 	        } else {
+	// 	        	$('.gnb_a').removeClass('active-color');
+	// 	        }
+	// 		});		
+	// 	}
+	// }).resize();
+
 	$(window).resize(function () {
+		console.log(vh(100));
+		$(window).on("scroll", function () {
 
-		if (window.innerWidth < 1024) {
-
-			$(window).on("scroll", function () {
+			if (window.innerWidth < 1024) {
 
 				var scroll = $(window).scrollTop();
 
 				if (scroll > 100) {
 					$('header').css('background','#fff');
-				}
-				else {
+				} else {
 					$('header').css('background','none');
 				}
 
-			});
-		} else {
-			$('header').css('background','none');
-		}
+			} else {
+				if (scroll > 600) {
+	        		$('.gnb_a').css('background','#e3dffd');
+		        } else {
+		        	$('.gnb_a').css('background','none');
+		        }
+
+		        $('header').css('background','none');
+			}
+		});	
 	}).resize();
+
 
 });
 
