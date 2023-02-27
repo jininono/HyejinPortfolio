00e85826b4e7f77e09309791acccceaadfbea9c1
jininono <jininono96@gmail.com>
2023-02-27 17:16:25 +0900

    ios vh

--- js/index.js
+++ js/index.js
@@ -74,6 +74,9 @@ $(function () {
 	}).resize();
 
 	//vh ios 버그 방지
+	let vh = window.innerHeight * 0.01;
+	document.documentElement.style.setProperty('--vh', `${vh}px`);
+
 	window.addEventListener('resize', () => {
 		let vh = window.innerHeight * 0.01;
 		document.documentElement.style.setProperty('--vh', `${vh}px`);
